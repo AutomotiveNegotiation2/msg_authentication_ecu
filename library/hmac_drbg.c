6aaf133b5bea89829ba20c6593294f84628e9b00
sjkim01 <sjkim@autocrypt.io>
2023-12-26 09:25:40 +0900

    apply alignment for the declaration statements. These will enhance readability

--- library/hmac_drbg.c
+++ library/hmac_drbg.c
@@ -190,11 +190,11 @@ static int mbedtls_hmac_drbg_finish_dbg(mbedtls_hmac_drbg_context *ctx,
                                         const unsigned char *additional,
                                         size_t add_len)
 {
-    size_t md_len = 0u;
-    uint8_t rounds = ((additional != NULL) && (add_len != 0)) ? 2 : 1;
-    uint8_t sep[2];
-    uint8_t K[MBEDTLS_MD_MAX_SIZE];
-    int32_t ret = MBEDTLS_ERR_MD_BAD_INPUT_DATA;
+    size_t      md_len      = 0u;
+    uint8_t     rounds      = ((additional != NULL) && (add_len != 0)) ? 2 : 1;
+    uint8_t     sep[2];
+    uint8_t     K[MBEDTLS_MD_MAX_SIZE];
+    int32_t     ret         = MBEDTLS_ERR_MD_BAD_INPUT_DATA;
 
     if ((ctx != NULL) && (additional != NULL) && (add_len > 0u))
     {
