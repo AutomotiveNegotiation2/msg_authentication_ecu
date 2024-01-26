9f9b11f8e64a9e58596f8edda088cd2c440985fa
Hangkook Cho <hkcho@autocrypt.io>
2024-01-26 11:50:27 +0900

    change default pub len of csr der internal to 100

--- library/x509write_csr.c
+++ library/x509write_csr.c
@@ -245,7 +245,7 @@ static int x509write_csr_der_internal(mbedtls_x509write_csr *ctx,
     size_t sig_oid_len = 100;
     unsigned char *c, *c2;
     unsigned char hash[MBEDTLS_HASH_MAX_SIZE];
-    size_t pub_len = 0, sig_and_oid_len = 0, sig_len;
+    size_t pub_len = 100, sig_and_oid_len = 0, sig_len;
     size_t len = 10000;
     mbedtls_pk_type_t pk_alg;
 #if defined(MBEDTLS_USE_PSA_CRYPTO)
