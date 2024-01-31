563d6a74c58bf65273d7635483799feb8ea3f5c5
lbd <bdlee2014@gmail.com>
2024-01-31 09:57:26 +0900

    edit x509_csr_parse_attributes

--- library/x509_csr.c
+++ library/x509_csr.c
@@ -246,6 +246,10 @@ static int x509_csr_parse_attributes(mbedtls_x509_csr *csr,
         return MBEDTLS_ERROR_ADD(MBEDTLS_ERR_X509_INVALID_EXTENSIONS,
                                  MBEDTLS_ERR_ASN1_LENGTH_MISMATCH);
     }
+    else 
+    {
+        return;
+    }
 
     return 0;
 }
